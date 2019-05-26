766e751b6bf62f2394093514f0169e23f5b17324
Nzix <iminezn5656@outlook.com>
2019-05-27 02:10:01 +0800

    optimize

--- background.js
+++ background.js
@@ -1,35 +1,29 @@
-var mode
+let mode
 
-chrome.webRequest.onBeforeSendHeaders.addListener(function (details) {
-	var header = {
-		name: 'X-Real-IP',
-		value: '118.88.88.88'
+chrome.webRequest.onBeforeSendHeaders.addListener(details => {
+	if (details.url.includes('163')) {
+		if (mode > 0) details.requestHeaders.push({
+			name: 'X-Real-IP',
+			value: '118.88.88.88'
+		})
 	}
-	if (mode > 0) details.requestHeaders.push(header)
-	return { requestHeaders: details.requestHeaders }
-}, { urls: ['*://music.163.com/*'] }, ['blocking', 'requestHeaders'])
+	return {requestHeaders: details.requestHeaders}
+}, {urls: ['*://music.163.com/*']}, ['blocking', 'requestHeaders'])
 
-// chrome.webRequest.onBeforeRequest.addListener(function (details) {
-// 	var redirectUrl = details.url.replace(
-// 		/(m\d+?)(?!c)\.music\.126\.net/, '$1c.music.126.net'
-// 	)
-// 	if (redirectUrl != details.url && mode > 1) return { redirectUrl: redirectUrl }
-// }, { urls: ['*://*.music.126.net/*'] }, ['blocking'])
-
-chrome.browserAction.onClicked.addListener(function (tab) {
+chrome.browserAction.onClicked.addListener(() => {
 	mode = (mode + 1) % 3
-	setMode()
+	sync()
 })
 
-chrome.storage.local.get('mode', function (data) {
-	mode = (typeof(data.mode) === 'undefined') ? 2 : data.mode
-	setMode()
+chrome.storage.local.get('mode', data => {
+	mode = data.mode == null ? 2 : data.mode
+	sync()
 })
 
-function setMode () {
-	chrome.storage.local.set({ mode: mode })
-	var titles = ['closed', 'normal', 'enhanced']
-	var icons = ['images/grey.svg', 'images/red.svg', 'images/blue.svg']
-	chrome.browserAction.setIcon({ path: icons[mode] })
-	chrome.browserAction.setTitle({ title: chrome.i18n.getMessage('name') + ' ' + '[' + chrome.i18n.getMessage(titles[mode]) + ']' })
+const sync = () => {
+	chrome.storage.local.set({mode: mode})
+	const title = ['closed', 'normal', 'enhanced']
+	const icon = ['images/grey.svg', 'images/red.svg', 'images/blue.svg']
+	chrome.browserAction.setIcon({path: icon[mode]})
+	chrome.browserAction.setTitle({title: `${chrome.i18n.getMessage('name')} [${chrome.i18n.getMessage(title[mode])}]`})
 }
--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 // https://stackoverflow.com/questions/9263671/google-chrome-application-shortcut-how-to-auto-load-javascript/9310273#9310273
 // https://stackoverflow.com/questions/9515704/insert-code-into-the-page-context-using-a-content-script/9517879#9517879
 
-chrome.storage.local.get('mode', function (data) {
-	mode = (typeof(data.mode) === "undefined") ? 2 : data.mode
-	if (mode == 2) inject()
+chrome.storage.local.get('mode', data => {
+	let mode = data.mode == null ? 2 : data.mode
+	if(mode == 2) inject()
 })
 
-function inject () {
-	var script = (document.head || document.documentElement).appendChild(document.createElement('script'))
+const inject = () => {
+	let script = (document.head || document.documentElement).appendChild(document.createElement('script'))
 	script.src = chrome.extension.getURL('inject.js')
-	script.onload = function() {
-		script.parentNode.removeChild(script)
-	}
+	script.onload = () => script.parentNode.removeChild(script)
 }
-
